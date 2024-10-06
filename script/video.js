4fe8bab0d864ef9ada2eabc6c5afdf26bf8367b1
PavelGoblin <pvlbrhm@gmail.com>
2024-10-07 00:58:53 +0600

    In Order

---
+++ script/video.js
@@ -0,0 +1,19 @@
+console.log("Video Added");
+
+//1- fetch ,load and show catagories on html
+
+//Create loadCatagories
+const loadCatagories = () => {
+  //fetch the data
+  fetch( https://openapi.programming-hero.com/api/phero-tube/categories)
+    .then((res) =>res.json())
+    .then((data)=>displayCatagories(data.catagories))
+    .catch((error) =>console.log(error));
+};
+
+//create DisplayCatagories
+const displayCatagories=(data) =>{
+  //add data in html
+  console.log(data);
+}
+loadCatagories();
